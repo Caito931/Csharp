fc2770879d2e1d63e07d3ff25b43f0e496c8febf
Caito931 <chora2aum@gmail.com>
2025-04-26 10:39:31 -0400

    Lets Learn Csharp!

---
+++ MyProject/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyProject 
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
