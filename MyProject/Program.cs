e7d3c0f4b2fa16e3bfc91a5ec141e1cfa283dd58
Caito931 <chora2aum@gmail.com>
2025-04-26 16:37:35 -0400

    Lesson 6

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -8,28 +8,16 @@ namespace MyProject
     {
         static void Main()
         {
-            // Type Casting
+            // User Input
 
-            double a = 3.14;
-            int b = Convert.ToInt32(a);
+            Console.WriteLine("What's your name?");
+            String name = Console.ReadLine();
 
-            int c = 123;
-            double d = Convert.ToDouble(c) + 0.1;
+            Console.WriteLine("What's your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
 
-            int e = 321;
-            String f = Convert.ToString(e);
-
-            String g = "$";
-            char h = Convert.ToChar(g);
-
-            String i = "true";
-            bool j = Convert.ToBoolean(i);
-
-            Console.WriteLine(b.GetType());
-            Console.WriteLine(d.GetType());
-            Console.WriteLine(f.GetType());
-            Console.WriteLine(h.GetType());
-            Console.WriteLine(j.GetType());
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your are " + age + " years old");
 
 
             Console.ReadKey();
--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1936bc75e55c0f7c06ca80e6f02975e585e0db7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba31233d4b513eadf1f1ae3d98fbd90ce516d519")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
