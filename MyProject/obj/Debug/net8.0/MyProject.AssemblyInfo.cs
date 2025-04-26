d2806dc0d7eb56236137ed725bc03cac6f7198d8
Caito931 <chora2aum@gmail.com>
2025-04-26 19:12:01 -0400

    Lesson 12

--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f6e18e8a99729578a90d6885557c9d733a62d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf2eb7b4e135e828b014f1903fb2b75c922009a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
