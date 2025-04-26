8e0a15a7c6790c5844cad15494280e60eebadd4d
Caito931 <chora2aum@gmail.com>
2025-04-26 16:25:06 -0400

    Lesson 4

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -8,29 +8,10 @@ namespace MyProject
     {
         static void Main()
         {
-            int x; // Declaration
-            x = 123; // Initialization
+            // Constants
+            const double PI = 3.14159;
 
-            int y = 321; // Declaration && Initialization
-
-            int z = x + y;
-
-            int age = 21; // whole integer
-            double height = 300.5; // decimal number
-            bool alive = true; // true or false
-            char symbol = '@'; // character
-            String name = "Caio"; // string (multiple characters)
-            String userName = symbol + name;
-
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine("Your age is " + age);
-            Console.WriteLine("Your height is " + height + " cm");
-            Console.WriteLine("Are you alive?: " + alive);
-            Console.WriteLine("Your symbil is: " + symbol);
-            Console.WriteLine("Hello " + name);
-            Console.WriteLine("Your username is: " + userName);
+            Console.WriteLine(PI);
 
             Console.ReadKey();
         }
