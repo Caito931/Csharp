333507bf7c19cfa1cbb01dbac9b2ada2e080f04d
Caito931 <chora2aum@gmail.com>
2025-04-26 23:06:35 -0400

    Lesson 13

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -8,36 +8,18 @@ namespace MyProject
     {
         static void Main()
         {
-            // Switch
-            Console.WriteLine("What day is it today?: ");
-            String day = Console.ReadLine();
+            // Logical Operators
 
-            switch(day) 
+            Console.WriteLine("What's the temperature outside: (C)");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            if (temp >= 10 && temp <= 25)
             {
-                case "Monday":
-                    Console.WriteLine("It's Monday!");
-                break;
-                case "Tuesday":
-                    Console.WriteLine("It's Tuesday!");
-                break;
-                case "Wednesday":
-                    Console.WriteLine("It's Wednesday!");
-                break;
-                case "Thursday":
-                    Console.WriteLine("It's Thursday!");
-                break;
-                case "Friday":
-                    Console.WriteLine("It's Friday!");
-                break;
-                case "Saturday":
-                    Console.WriteLine("It's Saturday!");
-                break;
-                case "Sunday":
-                    Console.WriteLine("It's Sunday!");
-                break;
-                default:
-                    Console.WriteLine(day + " is not a day!");
-                break;
+                Console.WriteLine("It's warm outside!");
+            }
+            else if (temp <= -50 || temp >= 50)
+            {
+                Console.WriteLine("Do not go outside!");
             }
 
             Console.ReadKey();
--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf2eb7b4e135e828b014f1903fb2b75c922009a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2806dc0d7eb56236137ed725bc03cac6f7198d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
