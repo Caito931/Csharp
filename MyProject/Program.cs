faa78ae88197e9a09fbc1c86ae682b53fb56e198
Caito931 <chora2aum@gmail.com>
2025-04-26 23:21:03 -0400

    Lesson 15

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,29 +1,25 @@
 ﻿
 using System;
 using System.Net.WebSockets;
-
+using System; 
+using System.Net.WebSockets; 
 namespace MyProject 
 {
     class Program
     {
         static void Main()
         {
-            // Logical Operators
-
-            Console.WriteLine("What's the temperature outside: (C)");
-            double temp = Convert.ToDouble(Console.ReadLine());
-
-            if (temp >= 10 && temp <= 25)
+            // While loop
+            String name = "";
+            while (name == "")
             {
-                Console.WriteLine("It's warm outside!");
-            }
-            else if (temp <= -50 || temp >= 50)
-            {
-                Console.WriteLine("Do not go outside!");
-            }
-
-            Console.ReadKey();
-        }
-    }
+                Console.Write("Enter your name: "); 
+                name = Console.ReadLine();
+            } 
+            
+            Console.WriteLine("Hello " + name);
+            
+            Console.ReadKey(); 
+        } 
+    } 
 }
-
--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2806dc0d7eb56236137ed725bc03cac6f7198d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+333507bf7c19cfa1cbb01dbac9b2ada2e080f04d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
