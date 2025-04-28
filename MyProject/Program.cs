d66130e381732497628209da6780f4905c8e6708
Caito931 <chora2aum@gmail.com>
2025-04-28 16:36:02 -0400

    Lesson 28

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -10,40 +10,15 @@ namespace MyProject
     {
         static void Main()
         {
-            // Exception & try catch
+            // Conditional Operator
+            // (condition) ? x : y
 
-            double x;
-            double y;
-            double result;
+            double temperature = 20;
+            String message;
 
-            try
-            {
-                Console.Write("Enter number 1: ");
-                x = Convert.ToInt32(Console.ReadLine());
+            message = (temperature >= 15) ? "It's warm outside!" : "It's cold outside!";
 
-                Console.Write("Enter number 2: ");
-                y = Convert.ToInt32(Console.ReadLine());
-
-                result = x / y;
-
-                Console.WriteLine("Result: " + result);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Enter only numbers please.");
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine("You can't divide by zero!");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Something went wrong");
-            }
-            finally
-            {
-                Console.WriteLine("Thanks for visiting");
-            }
+            Console.WriteLine(message);
 
             Console.ReadKey(); 
         }
--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27373222e58387d04443c4ae18c6e7d03947d26d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9114dad6ee87c1da06078d5de8a72b7f5d6e6f39")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
