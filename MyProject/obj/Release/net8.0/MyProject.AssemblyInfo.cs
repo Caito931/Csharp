81a5fb7236428ccabd98291b099351171a96c4c6
Caito931 <chora2aum@gmail.com>
2025-04-28 17:07:59 -0400

    Lesson 31

--- MyProject/obj/Release/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Release/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d5c81096874c47151ae21a182e6a6023dc2b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb1aa8d8206425b30814d7b455520d4e671814e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
